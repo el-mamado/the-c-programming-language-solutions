acb35a560983bd7ef573f630e5ae5f19ff3c64b5
el-mamado <m1100000moustafa@gmail.com>
2019-07-24 01:35:48 +0200

    Ex 1-17

--- Chapter-1/Ex1-17/Ex1-17.c
+++ Chapter-1/Ex1-17/Ex1-17.c
@@ -1,47 +1,35 @@
 /*
-Author: el-mamado
-Exercise 1-17. Write a program to print all input lines that are longer than 80 characters.
+Exercise 1-18. Write a program to remove trailing blanks and tabs from each line of input, and to delete entirely blank lines.
 */
 
 #include <stdio.h>
 
 #define MAXLENGTH 1000
 
-int getLineLength(char line[]);
-void printLine(char line[]);
+int getline(char s[], int max);
 
 int main() {
 	int min_length = 80;
 	int current_length;
 	char line[MAXLENGTH];
 
-	while ((current_length = getLineLength(line)) > 0) {
+	while ((current_length = getline(line, MAXLENGTH)) > 0) {
 		if (current_length > min_length) {
-			printLine(line);
+			printf("%s", line);
 		}
 	}
 }
 
-/* getLineLength: read a line into s, return length */
-int getLineLength(char line[]) {
-	int c, i;
-
-	for (i = 0; i < MAXLENGTH - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
-		line[i] = c;
+/* getline: read a line into s, return length */
+int getline(char s[], int max) {
+	int i, c;
+	for (i = 0; i < max - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+		s[i] = c;
 	}
-
 	if (c == '\n') {
-		line[i] = c;
+		s[i] = c;
 		i++;
 	}
-
-	line[i] = '\0';  // add string terminating character.
-	return i; // the length of the read line.
-}
-
-void printLine(char line[]) {
-
-	for (int i = 0; line[i] != '\0'; i++) {
-		printf("%c", line[i]);
-	}
+	s[i] = '\0';
+	return i;
 }
